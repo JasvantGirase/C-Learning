9f6eec49a1668e54ffaf9bf49be41a356049f714
JasvantGirase <girasejasvant22@gmail.com>
2026-07-28 11:11:09 +0530

    Create relational.c

---
+++ operators/relational.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// int main()
+// {
+// int a = 90;
+// int b = 8;
+// printf("%d\n", a > b); /// 90>8// true ==1
+// printf("%d\n", a < b); // 90<8 /// 0
+// printf("%d\n", a == b); /// 90==8///f//e
+// printf("%d\n", a != b); /// 901-8 /// t ///1
+// printf("%d\n", a >= b); /// 90>=8 1
+// printf("%d\n", a <= b); /// 90<=8 0
+// }
