77825b4752abe139d5ad42af74b2dd497513d965
JasvantGirase <girasejasvant22@gmail.com>
2026-07-28 12:18:31 +0530

    Create bitwise.c

---
+++ operators/bitwise.c
@@ -0,0 +1,61 @@
+// #include <stdio.h>
+// int main()
+// {   // bitwise and
+    // int a = 13;
+    // int b = 9;
+    // printf("%d & %d :- %d",a,b,a & b);
+
+    //bitwise or
+    //  int a = 12;
+    //  int b = 7;
+    //  printf("%d | %d :- %d",a,b,a | b);
+
+    //bitwise xor
+    //  int a = 15;
+    // int b = 10;
+    // printf("%d",15^10);
+
+    //left shift 
+    // printf("%d",15<<2);
+
+    //right shift
+    // printf("%d",15>>1);
+    // printf("%d",15>>2);
+    // printf("%d",15>>3);
+    // printf("%d",15>>4);
+
+    //togel(~)
+//    #include <stdio.h>
+
+// int main()
+// {
+//     int a = 5;
+//     printf("%d", a ^ (1 << 3));
+
+//     return 0;
+// }
+
+    //set bit 1 of 13
+//int a = 13; //1101// set bit 1//1111 = 15
+//printf("%d",a |(1<<1));//15
+    
+//set bit 3rd of 13
+// int a = 13; //1101// set bit 3//1101 = 13
+// printf("%d",a |(1<<3));//13
+//     //
+
+//set bit 1st of 4
+// int a = 4; 
+//  printf("%d",a |(1<<1));
+
+//clear 3rd bit of 12
+// int a =12;
+// printf("%d",a & ~(1<<3));
+
+// clear 0 th number of 15
+// int a = 15;
+// printf("%d",a & ~(1<<0));
+
+//toggle 3rd bit of 5
+//  int a = 5;
+//  printf("%d",a ^ (1<<3));
