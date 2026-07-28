6bbc4980648356daf5e5499948935b835e5624e5
JasvantGirase <girasejasvant22@gmail.com>
2026-07-29 02:03:51 +0530

    Create union.c

---
+++ union/union.c
@@ -0,0 +1,44 @@
+//ex1
+// #include <stdio.h>
+
+// union ID
+// {
+//     int adhar;
+//     int pan;
+//     int passport;
+// } u1;
+
+// int main()
+// {
+//     u1.adhar = 12345;
+
+//     printf("%d\n", u1.adhar);
+//     printf("%d\n", u1.pan);
+
+//     return 0;
+// }
+
+
+//ex2
+// #include <stdio.h>
+
+// union ID
+// {
+//     int adhar;
+//     int pan;
+//     int passport;
+// } u1;
+
+// int main()
+// {
+//     u1.adhar = 12345;
+//     u1.pan = 908765;
+
+//     printf("%d\n", u1.adhar);
+//     printf("%d\n", u1.pan);
+//     printf("%d\n", u1.passport);
+
+//     printf("size of union %d", sizeof(u1));
+
+//     return 0;
+// }
