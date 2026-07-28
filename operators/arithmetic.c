80fd0b0aac3573bea933dc06ea71f15ebc8cb688
JasvantGirase <girasejasvant22@gmail.com>
2026-07-28 11:08:26 +0530

    Create arithmetic.c

---
+++ operators/arithmetic.c
@@ -0,0 +1,21 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a = 15;
+//     int b = 5;
+
+//     printf("Addition of %d and %d is: %d\n", a, b, a + b);
+
+//     printf("Subtraction of %d and %d is: %d\n", a, b, a - b);
+
+//     printf("Multiplication of %d and %d is: %d\n", a, b, a * b);
+
+//     printf("Division of %d and %d is: %d\n", a, b, a / b);
+
+//     printf("Mod of %d and %d is: %d\n", a, b, a % b);
+
+//     printf("%d\n", 45 % 4);
+
+//     return 0;
+// }
