e16697e59b99204026ec3de3350c84151e4601b1
JasvantGirase <girasejasvant22@gmail.com>
2026-07-28 11:07:38 +0530

    Create ternary.c

---
+++ operators/ternary.c
@@ -0,0 +1,17 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 90;
+//     int b = 56;
+
+//     int c =(a > b) ?a: b;
+//     printf("%d", c);
+
+    
+//     int c =(a < b) ?a: b;
+//     printf("%d", c);
+
+//     int c = (a > b) ? printf("largest number is: %d", a) :printf("largest number is: %b",b);
+
+
+// }
