33ab96e27d022885b6cba2da42260d9c3fd8caa1
JasvantGirase <girasejasvant22@gmail.com>
2026-07-28 11:11:21 +0530

    Create logical.c

---
+++ operators/logical.c
@@ -0,0 +1,43 @@
+//ex1
+// #include <stdio.h>
+
+// int main()
+// {
+//     // AND operator
+//     char c = '7';
+
+//     int r = (c > '1' && c < '5');
+
+//     printf("%d\n", r);
+
+//     return 0;
+// }
+
+//ex2
+// #include <stdio.h>
+
+// int main()
+// {
+//     // OR operator
+//     char c = '7';
+
+//     int r = (c > '5' || c < '3');
+
+//     printf("%d\n", r);
+
+//     return 0;
+// }
+
+//ex3
+// #include<stdio.h>
+// int main()
+// {
+//     char c = '2';
+
+//     int r = !(c > '5');
+
+//     printf("%d\n", r);
+
+//     return 0;
+// }
+
