c1dbda1ce394a2ad58a8cdadb16bda989e2d09e8
JasvantGirase <girasejasvant22@gmail.com>
2026-07-28 12:18:36 +0530

    Create inlinefunc.c

---
+++ function/inlinefunc.c
@@ -0,0 +1,13 @@
+// #include<stdio.h>
+// static inline void swap(int a,int b)
+// {
+//     int temp=a;
+//     a=b;
+//     b=temp;
+//     printf("%d %d",a,b);
+
+// }
+// int main()
+// {
+//     swap(10,99);
+// }
