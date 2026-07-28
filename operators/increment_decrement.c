c3c41c6435f2eb5ad374be3f11d638cdfaa3ca8e
JasvantGirase <girasejasvant22@gmail.com>
2026-07-28 11:11:30 +0530

    Create increment_decrement.c

---
+++ operators/increment_decrement.c
@@ -0,0 +1,34 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     // Pre-increment
+//     int x = 10;
+
+//     printf("Pre-increment:\n");
+//     printf("%d\n", ++x);   // First increases, then prints
+//     printf("%d\n", x);     // Value becomes 11
+
+//     // Post-increment
+//     int y = 20;
+
+//     printf("\nPost-increment:\n");
+//     printf("%d\n", y++);   // First prints, then increases
+//     printf("%d\n", y);     // Value becomes 21
+
+//     // Pre-decrement
+//     int s = 10;
+
+//     printf("\nPre-decrement:\n");
+//     printf("%d\n", --s);   // First decreases, then prints
+//     printf("%d\n", s);     // Value becomes 9
+
+//     // Post-decrement
+//     int a = 78;
+
+//     printf("\nPost-decrement:\n");
+//     printf("%d\n", a--);   // First prints, then decreases
+//     printf("%d\n", a);     // Value becomes 77
+
+//     return 0;
+// }
