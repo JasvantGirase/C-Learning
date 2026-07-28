9c34808ea90440a0605c9b3955868d94d620f6ed
JasvantGirase <girasejasvant22@gmail.com>
2026-07-28 11:17:11 +0530

    Create function.c

---
+++ function/function.c
@@ -0,0 +1,103 @@
+//ex1
+// #include <stdio.h>
+
+// // Function declaration (Prototype)
+// void add();
+
+// int main()
+// {
+//     // Function calls
+//     add();
+//     add();
+//     add();
+
+//     return 0;
+// }
+
+// // Function definition
+// void add()
+// {
+//     int a = 90;
+//     int b = 10;
+//     int c = a + b;
+
+//     printf("%d\n", c);
+// }
+
+
+//ex2 with return type with argument
+// #include <stdio.h>
+
+// int doubleNumber(int);
+
+// int main()
+// {
+//     int result = doubleNumber(5);
+
+//     printf("%d", result);
+
+//     return 0;
+// }
+
+// int doubleNumber(int num)
+// {
+//     return num * 2;
+// }
+
+
+
+//ex3 with return type with argument
+// #include <stdio.h>
+
+// int mul(int, int);
+
+// int main()
+// {
+//     int m = mul(10, 5);
+
+//     printf("%d", m);
+
+//     return 0;
+// }
+
+// int mul(int a, int b)
+// {
+//     int c = a * b;
+
+//     return c;
+// }
+
+
+
+
+//ex4 with parameter without return type
+// #include <stdio.h>
+// void div(int, int); /// declaration
+// int main()
+// {
+// div(10, 5);/// fun call
+// div(78, 5);/// call
+// }
+// void div(int a, int b)
+// {
+// int c=a / b;
+// printf("%d\n", c);
+// }
+
+//ex5 without parameter with return type
+// #include <stdio.h>
+// int mod(); /// declaration
+// int main()
+// {
+// int m = mod();
+// printf("%d", m);
+// }
+// int mod()
+// {
+// int a = 90;
+// int b = 4;
+// int c = a % b;
+// return c;
+// }
+
+
