7080632c113817a8a384a9224f65bea8d5fe49c4
JasvantGirase <girasejasvant22@gmail.com>
2026-07-29 02:46:42 +0530

    Create enum.c

---
+++ enumeration/enum.c
@@ -0,0 +1,60 @@
+//ex1
+// #include <stdio.h>
+
+// enum Day
+// {
+//     MON,
+//     TUE,
+//     WED
+// };
+
+// int main()
+// {
+//     enum Day d = TUE;
+
+//     printf("%d", d);
+
+//     return 0;
+// }
+
+
+
+
+//ex2
+// #include <stdio.h>
+
+// enum Status
+// {
+//     OFF = 0,
+//     ON = 1
+// };
+
+// int main()
+// {
+//     enum Status s = ON;
+
+//     printf("%d", s);
+
+//     return 0;
+// }
+
+
+
+//ex3
+// #include <stdio.h>
+
+// enum Color
+// {
+//     RED = 10,
+//     GREEN,
+//     BLUE
+// };
+
+// int main()
+// {
+//     printf("%d\n", RED);
+//     printf("%d\n", GREEN);
+//     printf("%d\n", BLUE);
+
+//     return 0;
+// }
