edb49cd779ce20bdde586e229ecb9ba788e23c38
JasvantGirase <girasejasvant22@gmail.com>
2026-08-02 00:02:26 +0530

    Create typedef.c

---
+++ typedef/typedef.c
@@ -0,0 +1,23 @@
+//ex1
+//typedef the structure
+// #include<stdio.h>
+// #include<string.h>
+// struct student
+// {
+//     int roll;
+//     char name [10];
+// };
+// int main()
+// {
+//     typedef struct student stu;
+//     stu s1;
+//     s1.roll=90;
+//     strcpy(s1.name,"tevhno");
+//     printf("%d\n",s1.roll);
+//     printf("%s",s1.name);
+
+// }
+
+
+//typedef mostly used in structure becuase some 
+//structure have big names
